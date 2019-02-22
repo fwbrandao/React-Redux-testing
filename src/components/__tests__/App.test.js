50616c6a4cd4135c3e46bee967425dbcb095f1cc
Fernando Brandao <fernando.brandao@adarga.ai>
2019-02-22 15:44:33 +0000

    cleaner test suite

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,18 +4,18 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
+let wrapper;
+
+beforeEach(() => {
+    // shallow only render app component, not any children.
+    wrapper = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-    // shallow only render app component, not any children.
-    const wrapper = shallow(<App />);
-
     expect(wrapper.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', () => {
-    // shallow only render app component, not any children.
-    const wrapper = shallow(<App />);
-
     expect(wrapper.find(CommentList).length).toEqual(1);
 });
 

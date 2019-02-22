573613f49a713bca3951b10fb62dd35df51e38e0
Fernando Brandao <fernando.brandao@adarga.ai>
2019-02-22 15:40:34 +0000

    added .env file

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-import "../../setupTests";
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 
 it('shows a comment box', () => {
+    // shallow only render app component, not any children.
     const wrapper = shallow(<App />);
 
     expect(wrapper.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+    // shallow only render app component, not any children.
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find(CommentList).length).toEqual(1);
 });
 

706a60a62a2438a1825f47358d900912823eb160
Fernando Brandao <fernando.brandao@adarga.ai>
2019-02-22 08:32:38 +0000

    test commentBox

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import CommentBox from '../CommentBox';
+import "../../setupTests";
+
+
+it('shows a comment box', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find(CommentBox).length).toEqual(1);
+});
+

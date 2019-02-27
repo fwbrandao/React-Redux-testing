87c6a514d0a6bacf07f575850440d1074e629bb3
Fernando Brandao <fwbrandao@gmail.com>
2019-02-27 21:10:09 +0000

    textarea test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,5 +16,17 @@ it('has a text area and a button', () => {
     // then
     expect(wrapped.find('textarea').lenght).toEqual(1);
     expect(wrapped.find('button').lenght).toEqual(1);
-
 });
+
+it('has a text area that users can type in', () => {
+    // then
+    wrapped.find('textarea').simulate('change', {
+        target: {
+            value: 'new comment'
+        }
+    });
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+

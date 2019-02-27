d166b5849542701e68e30cedcdb1681c19b00eca
Fernando Brandao <fw>
2019-02-27 08:44:09 +0000

    github email setup

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -4,14 +4,17 @@ import CommentBox from 'components/CommentBox';
 
 let wrapped;
 
+// first
 beforeEach(() => {
     wrapped = mount(<CommentBox />);
 });
 
+// last
 afterEach(() => {
     wrapped.unmount();
 });
 
+// second
 it('has a text area and a button', () => {
     // then
     expect(wrapped.find('textarea').lenght).toEqual(1);

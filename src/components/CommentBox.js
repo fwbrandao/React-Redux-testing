25f58dd517808f66fadad61d6690927bf75a00ad
Fernando Brandao <fwbrandao@gmail.com>
2019-03-04 08:20:45 +0000

    list items been displayed

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
-import { saveComment } from 'actions/index';
 
 class CommentBox extends Component {
     state = {

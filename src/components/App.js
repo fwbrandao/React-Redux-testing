ce305f6e2383d0dd0face9c8ce5722290be54bfb
Fernando Brandao <fwbrandao@gmail.com>
2019-03-07 08:42:45 +0000

    sign in button

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import { connect } from 'react-redux';
 
-export default () => {
-    return (
-        <div>
-            <CommentBox />
-            <CommentList />
-        </div>
+class App extends Component {
+    renderButton() {
+        if(this.props.auth) {
+           return  <button>Sign out</button>;
+        } else {
+           return <button>Sign in</button>;
+        }
+    }
 
-    );
+    renderHeader() {
+        return (
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/post'>Post a Comment</Link>
+                </li>
+                <li>
+                    <Link to='/'>{this.renderButton}</Link>
+                </li>
+            </ul>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderHeader()}
+                <Route path='/post' component={CommentBox} />
+                <Route path='/' exact component={CommentList} />
+            </div>
+        );
+    }
 };
+
+function mapStateToProps(state) {
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps) (App);
